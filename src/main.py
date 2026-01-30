bd49c8dc509ae33829c771dbc59265ce462827d0
root <gitlab@factentry.com>
2026-01-30 18:24:19 +0530

    fix version changed

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ from prometheus_client import Counter, Histogram, generate_latest
 # App metadata
 # -----------------------------------------------------------------------------
 APP_NAME = "fastapi-demo"
-APP_VERSION = os.getenv("APP_VERSION", "v2.0.2")
+APP_VERSION = os.getenv("APP_VERSION", "v2.0.3")
 BUILD_TIME = os.getenv("BUILD_TIME", "unknown")
 START_TIME = time.time()
 
